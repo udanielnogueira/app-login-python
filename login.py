1c7c31e9bb06a37d0ed73c61c220c087e41db530
Daniel Nogueira <danielsilvangr@gmail.com>
2024-09-22 15:59:47 -0300

    Refatora código

--- login.py
+++ login.py
@@ -1,55 +1,59 @@
 from PySimpleGUI import PySimpleGUI as sg
 
 # Layout do login
-sg.theme('Dark Grey 13')
+sg.theme("Dark Grey 13")
 layout = [
-    [sg.Text('Usuário'), sg.Input(key='usuario', size=(20,1))], 
-    [sg.Text('Senha  '), sg.Input(key='senha', password_char='*', size=(20,1))], 
-    [sg.Checkbox('Salvar o login?')], 
-    [sg.Button('Entrar')] 
+    [sg.Text("Usuário"), sg.Input(key="usuario", size=(20, 1))],
+    [sg.Text("Senha  "), sg.Input(key="senha", password_char="*", size=(20, 1))],
+    [sg.Checkbox("Salvar o login?")],
+    [sg.Button("Entrar")],
 ]
 
 # Janela do login
-janela = sg.Window('Login', layout, size=(640, 480))
+janela = sg.Window("Login", layout, size=(640, 480))
 
 # Layout do sistema
-sg.theme('Dark Grey 13') # Reddit
+sg.theme("Dark Grey 13")
 layout2 = [
-    [sg.Text('Sistema funcionando...')],
+    [sg.Text("Sistema funcionando...")],
 ]
 
 # Janela do sistema
-janela2 = sg.Window('App', layout2, size=(640, 480))
+janela2 = sg.Window("App", layout2, size=(640, 480))
 
 # Eventos
 while True:
-    eventos, valores = janela.read() 
+    eventos, valores = janela.read()
     if eventos == sg.WIN_CLOSED:
         break
-    if eventos == 'Entrar':
-        # dados válidos
-        if valores['usuario'] == 'daniel' and valores['senha'] == '123':
-            
-            # passagem de valor para variável
-            nome = valores['usuario']
+    if eventos == "Entrar":
+        # Dados válidos
+        if valores["usuario"] == "daniel" and valores["senha"] == "123":
 
-            # pop up
-            sg.popup('Login realizado com sucesso!', f'Bem vindo ao sistema {nome.capitalize()}.', title=' ') # aceita pop de variáveis
-            
-            # fechando janela do login
+            # Passagem de valor para variável
+            nome = valores["usuario"]
+
+            # Pop-up
+            sg.popup(
+                "Login realizado com sucesso!",
+                f"Bem vindo ao sistema {nome.capitalize()}.",
+                title=" ",
+            )
+
+            # Fechando janela do login
             janela.close()
 
-            # abrindo janela do sistema
+            # Abrindo janela do sistema
             while True:
                 eventos, valores = janela2.read()
                 if eventos == sg.WIN_CLOSED:
                     break
-        
-        # dados inválidos
-        else:
-            sg.popup('Dados inválidos, tente novamente.', title=' ') 
 
-'''
+        # Dados inválidos
+        else:
+            sg.popup("Dados inválidos, tente novamente.", title=" ")
+
+"""
 debug output
 sg.Print('Erro')
-'''
+"""
